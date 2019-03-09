2c2023cbb210018371e572d77e3f9c4a59c44dee
Bao Pham <ryanvuepham@gmail.com>
2019-03-09 11:55:20 -0600

    Get endpoints are working

--- api/server.js
+++ api/server.js
@@ -1,10 +1,45 @@
-const express = require('express');
+const express = require('express')
 const server = express();
 
- server.use(express.json());
+const projectsDb = require('../data/helpers/projectModel')
+const actionsDb = require('../data/helpers/actionModel')
 
- server.get('/', (req, res) => {
-    res.json('alive');
+const parser = express.json()
+
+//server.use(json.express())
+server.use(parser)
+
+server.get('/', (req, res) => {
+  res.json('alive');
 })
 
- module.exports = server; 
+
+// Project endpoints 
+//GET = read
+server.get('/projects', (req, res) => {
+  projectsDb.get()
+  .then(projects => res.status(200).json(projects))
+  .catch(err => {
+      res.status(500).json({ message: "Couldn't retrieve projects" })
+  })
+})
+
+//GET by ID
+server.get('/projects/:id', (req, res) => {
+  projectsDb.get(req.params.id)
+  .then(project => res.status(200).json(project))
+  .catch(err => {
+      res.status(404).json({ message: "there is no user by this ID" })
+  })
+})
+
+//GET Project Actions
+server.get('/projects/:id/all', (req, res) => {
+  projectsDb.getProjectActions(req.params.id)
+  .then(projects => res.status(200).json(projects))
+  .catch(err => {
+      res.status(500).json({ message: "failed to get actions"})
+  })
+})
+
+module.exports = server; 
