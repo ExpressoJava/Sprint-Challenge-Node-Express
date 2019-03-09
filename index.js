0fa129d9ef935fb10c4c4617b3222e0ee4d8c9e1
Bao Pham <ryanvuepham@gmail.com>
2019-03-09 11:45:25 -0600

    added dependenicies, folders, server running

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 // play this: https://www.youtube.com/watch?v=d-diB65scQU
 
 // code away!
+const server = require('./api/server.js')
+
+const port = 7000
+
+server.listen(port, () => console.log(`Server is litening to port ${port}`))
